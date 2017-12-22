6e5c91f06f92619aff31a4946ce179246c1e42d2
Daniel Lennox <lennox.dan@gmail.com>
2017-12-22 19:09:06 +0100

    Added user logout endpoint.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,6 +13,13 @@ module.exports = (app) => {
   // Handle the return authentication code.
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // Handle logout request.
+  app.get('/api/user/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+
   // Test user authentication.
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
