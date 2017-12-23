8ce01710c030ce31f98d8c44344dac853494d9bb
Daniel Lennox <lennox.dan@gmail.com>
2017-12-23 15:43:39 +0100

    Allow proxy with google auth.

--- services/passport.js
+++ services/passport.js
@@ -44,7 +44,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // We only really need to store and use the access and refresh tokens when we
