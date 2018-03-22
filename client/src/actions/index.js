1f96aa1ccba9d581b20e8cd095a85c232395d297
Daniel Lennox <lennox.dan@gmail.com>
2018-03-22 16:49:17 -0700

    Implemented Survey List UI.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -24,6 +24,12 @@ export const submitSurvey = (values, history) => async dispatch => {
   history.push('/surveys');
   // update user model, since we will have now spent a credit.
   dispatch({ type: FETCH_USER, payload: res.data});
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({type: FETCH_SURVEYS, payload: res.data});
 };
 
 // Our Redux thunk middleware will catch the fact that we're returning a function
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,  // The 'auth' piece of state is managed by our authReducer.
-  form: reduxFormReducer
+  form: reduxFormReducer,
+  surveys: surveysReducer
 });
